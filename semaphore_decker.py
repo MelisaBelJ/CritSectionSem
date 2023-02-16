06e9623097a6aa5f885d51b2b148c01b3a23a77a
Melisa <melisabe@ucm.es>
2023-02-16 10:20:20 +0100

    Made semaphore blocking, removed giving up message in semaphore_decker.py

--- semaphore_decker.py
+++ semaphore_decker.py
@@ -1,26 +1,27 @@
-from multiprocessing import Process
-from multiprocessing import current_process
-from multiprocessing import Value, Array, BoundedSemaphore
+from multiprocessing import Process, current_process, Value, BoundedSemaphore
+from time import sleep
+from random import random
+
 N = 8
 def task(common, semaforo):
 	tid = current_process().name
-	for i in range(100):
-		print(f'{tid}−{i}: Non−critical Section')
-		print(f'{tid}−{i}: End of non−critical Section')
-		while not semaforo.acquire(False):
-			print(f'{tid}−{i}: Giving up')
-		print(f'{tid}−{i}: Critical section')
-		v = common.value + 1
-		print(f'{tid}−{i}: Inside critical section')
-		common.value = v
-		print(f'{tid}−{i}: End of critical section')
-		semaforo.release()
+	for i in range(20):
+		print(f'{tid}−{i}: Non−critical Section', flush = True)
+		print(f'{tid}−{i}: End of non−critical Section', flush = True)		
+		sleep(random())
+		with semaforo:
+			print(f'{tid}−{i}: Critical section', flush = True)
+			v = common.value + 1
+			print(f'{tid}−{i}: Inside critical section', flush = True)
+			sleep(random())
+			common.value = v
+			print(f'{tid}−{i}: End of critical section', flush = True)
 def main():
 	lp = []
 	common = Value('i', 0)
 	semaforo = BoundedSemaphore()
 	for tid in range(N):
-		lp.append(Process(target=task, name=tid, args=(common, semaforo)))
+		lp.append(Process(target=task, name=f'{tid}', args=(common, semaforo)))
 	print (f"Valor inicial del contador {common.value}")
 	for p in lp:
 		p.start()
